222720df508dbff09352b7f3d502d5bee8b844db
GAMEZ-slayer <rtesh9783@gmail.com>
2026-05-26 00:54:24 +0300

    Update login API endpoint to production URL

--- client/src/Login.js
+++ client/src/Login.js
@@ -21,7 +21,7 @@ const Login = ({ onLogin }) => {
     };
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', loginPayload);
+      const response = await axios.post('https://hams-lounge-4.onrender.com', loginPayload);
 
       if (response.data?.token) {
         localStorage.setItem('token', response.data.token);
