8aa9687dd8ccd4a97cce348e3bac17bd43a3af02
Roy <rtesh9783@gmail.com>
2026-05-30 10:47:10 +0300

    Fix axios.js merge conflict

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: ' http://localhost:5000', // Points to your unified backend
-=======
-  baseURL: 'https://hams-lounge-4.onrender.com', // Points to your unified backend
->>>>>>> 1dce468b843891395de8d9746ce7480ae6b4022d
+  baseURL: 'https://hams-lounge-4.onrender.com',
 });
 
-// Automatically attaches your secure token to every request header
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
